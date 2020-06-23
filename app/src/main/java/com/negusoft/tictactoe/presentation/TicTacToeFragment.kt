5665f32f65b3b0e4645c2e1bde3ee0bc137246fb
Richard Hendricks <richardhendricks@piedpiper.com>
2020-06-23 09:29:38 +0200

    Implement static view

---
+++ app/src/main/java/com/negusoft/tictactoe/presentation/TicTacToeFragment.kt
@@ -0,0 +1,20 @@
+package com.negusoft.tictactoe.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.negusoft.tictactoe.R
+
+class TicTacToeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tictactoe, container, false)
+    }
+
+}
